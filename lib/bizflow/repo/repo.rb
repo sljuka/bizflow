efb5ed0ee2a1a01b1980e8c1b5e45ac28625fea6
David Sljukic <david.sljukic@gmail.com>
2015-02-28 22:21:23 +0100

    text update

--- lib/bizflow/repo/repo.rb
+++ lib/bizflow/repo/repo.rb
@@ -1,6 +1,6 @@
 require "sqlite3"
 require 'sequel'
-require 'bizflow/business/process'
+require 'bizflow/business_model/process'
 
 module Bizflow
 
@@ -9,14 +9,14 @@ module Bizflow
     def initialize
       puts "connecting to #{db_path}"
       Sequel.connect("sqlite://#{db_path}")
-      Dir[File.expand_path("../../model/*.rb", __FILE__)].each { |file| require_relative file }
+      Dir[File.expand_path("../../data_model/*.rb", __FILE__)].each { |file| require_relative file }
     end 
 
     def create_process(blueprint_id, creator_id)
-      pbp = Bizflow::Model::ProcessBlueprint[blueprint_id]
+      pbp = Bizflow::DataModel::ProcessBlueprint[blueprint_id]
       raise "no process with id '#{blueprint_id}'" unless pbp
 
-      p = Bizflow::Model::Process.create(
+      p = Bizflow::DataModel::Process.create(
         name: pbp.name,
         description: pbp.description,
         creator_id: creator_id,
@@ -24,34 +24,34 @@ module Bizflow
       )
       
       pbp.action_blueprints.each do |bp|
-        a = Bizflow::Model::Action.create(name: bp.name, type: bp.type, process: p, action_blueprint: bp)
+        a = Bizflow::DataModel::Action.create(name: bp.name, type: bp.type, process: p, action_blueprint: bp)
         puts (pbp.start == a.name) ? "true" : "false"
         p.update(start_id: a.id) if pbp.start == a.name
       end
 
-      h = Bizflow::Model::Head.create(process: p)
+      h = Bizflow::DataModel::Head.create(process: p)
 
-      Bizflow::Business::Process.new p
+      Bizflow::BusinessModel::Process.new p
     end
 
     def process_blueprints
-      Bizflow::Model::ProcessBlueprint.all
+      Bizflow::DataModel::ProcessBlueprint.all
     end
 
     def processes
-      Bizflow::Model::Process.all
+      Bizflow::DataModel::Process.all
     end
 
     def find_process(id)
-      p = Bizflow::Model::Process[id]
-      Bizflow::Business::Process.new px
+      p = Bizflow::DataModel::Process[id]
+      Bizflow::BusinessModel::Process.new p
     end
 
     def tasks(user_id = nil)
       if user_id
-        Bizflow::Model::Task.where(assignee_id: user_id)
+        Bizflow::DataModel::Task.where(assignee_id: user_id)
       else
-        Bizflow::Model::Task.all
+        Bizflow::DataModel::Task.all
       end
     end
 
